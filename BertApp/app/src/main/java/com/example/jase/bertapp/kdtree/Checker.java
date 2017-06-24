b6a59bf146aba55a81c773fecb6f8fcc528593f5
Andy Bhadai <abhadai@gmail.com>
2017-06-24 23:26:52 +0200

    Dirty commit

--- BertApp/app/src/main/java/com/example/jase/bertapp/kdtree/Checker.java
+++
@@ -1,7 +0,0 @@
-package com.example.jase.bertapp.kdtree;
-
-public interface Checker<T> {
-
-    boolean usable(T v);
-
-}
--- BertApp/app/src/main/java/com/example/jase/bertapp/kdtree/distance/DistanceMetric.java
+++
@@ -1,7 +0,0 @@
-package com.example.jase.bertapp.kdtree.distance;
-
-public abstract class DistanceMetric {
-
-    public abstract double distance(double[] a, double[] b);
-
-}
