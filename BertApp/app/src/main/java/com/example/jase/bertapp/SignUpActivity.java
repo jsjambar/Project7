d17c54ce11476ef53a8bffe835380edbd0f4f468
Andy Bhadai <abhadai@gmail.com>
2017-06-26 19:57:13 +0200

    Removed unnecessary code

--- BertApp/app/src/main/java/com/example/jase/bertapp/SignUpActivity.java
+++ BertApp/app/src/main/java/com/example/jase/bertapp/SignUpActivity.java
@@ -42,24 +42,6 @@ public class SignUpActivity extends AppCompatActivity {
 
     public void createUser(){
 
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://takethestairs.today:3000";
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        Debug.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Debug.setText("That didn't work!");
-            }
-        });
-        queue.add(stringRequest);
-
         //this.Debug.setText(String.format("Username: %s, password: %s", this.PasswordField.getText().toString(), this.PasswordField.getText().toString()));
     }
 }
