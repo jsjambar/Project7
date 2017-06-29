e279f60e4dc84f4e83d6c7e3b220c6e812b281d4
Andy Bhadai <abhadai@gmail.com>
2017-06-29 12:59:49 +0200

    Started spinner

--- BertApp/app/src/main/java/com/example/jase/bertapp/SignUpActivity.java
+++ BertApp/app/src/main/java/com/example/jase/bertapp/SignUpActivity.java
@@ -3,8 +3,10 @@ package com.example.jase.bertapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.jase.bertapp.classes.User;
@@ -21,11 +23,21 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText password;
     private TextView debugText;
     private Button confirmButton;
+    private String[] PreferenceChoices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        this.PreferenceChoices = new String[]{
+                "Food and drinks", "Going out", "Relaxation"
+        };
+
+        Spinner s = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, this.PreferenceChoices);
+        s.setAdapter(adapter);
 
         this.username = (EditText) findViewById(R.id.txtUsername);
         this.password = (EditText) findViewById(R.id.txtPassword);
